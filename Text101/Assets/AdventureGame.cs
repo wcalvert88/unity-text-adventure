f95e739267fb9d116565a2f1495b9a0b9093e7b0
wcalvert88 <wcalvert88@gmail.com>
2018-09-14 14:08:23 -0400

    got the game states to change based on user input

--- Text101/Assets/AdventureGame.cs
+++ Text101/Assets/AdventureGame.cs
@@ -18,6 +18,28 @@ public class AdventureGame : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        ManageState();
 	}
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[3];
+        }
+        else
+        {
+            Debug.Log("You did not press 1, 2 or 3");
+        }
+        textComponent.text = state.GetStateStory();
+    }
 }
